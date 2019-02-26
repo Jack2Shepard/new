d42bf77ad7c958d005a56bb95149434175d83e40
Shepard <downread@gmail.com>
2019-02-26 22:44:15 +0330

    Hey

--- app/src/main/java/com/jash/shepard/threadexample/MainActivity.java
+++ app/src/main/java/com/jash/shepard/threadexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -16,6 +17,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     private String[] urls;
     private String urlToDownload;
     private ImageView mImageView;
@@ -55,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
             connection.connect();
             InputStream inputStream = connection.getInputStream();
             Bitmap bitmap  = BitmapFactory.decodeStream(inputStream);
+            Log.d(TAG, "getBitmapFromURL: " + bitmap);
             return bitmap;
         }catch (IOException e){
             Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
