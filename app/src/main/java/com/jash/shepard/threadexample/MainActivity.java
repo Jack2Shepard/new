602a608bb28d2166084800ae6c38624e074f40fe
Shepard <downread@gmail.com>
2019-02-27 21:09:21 +0330

    Hey

--- app/src/main/java/com/jash/shepard/threadexample/MainActivity.java
+++ app/src/main/java/com/jash/shepard/threadexample/MainActivity.java
@@ -2,6 +2,8 @@ package com.jash.shepard.threadexample;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,16 +19,23 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
     private String[] urls;
     private String urlToDownload;
     private ImageView mImageView;
     private Spinner mSpinner;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode
+                .ThreadPolicy
+                .Builder()
+                .permitAll()
+                .build();
+        StrictMode.setThreadPolicy(policy);
+
         inits();
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -47,29 +56,34 @@ public class MainActivity extends AppCompatActivity {
         mImageView = findViewById(R.id.image);
         mSpinner = findViewById(R.id.spinner);
         urls = getResources().getStringArray(R.array.urls);
+        mHandler = new Handler();
     }
 
-    private Bitmap getBitmapFromURL(String src){
-        try{
+    private Bitmap getBitmapFromURL(String src) {
+        try {
             URL url = new URL(src);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setDoInput(true);
             connection.connect();
             InputStream inputStream = connection.getInputStream();
-            Bitmap bitmap  = BitmapFactory.decodeStream(inputStream);
-            Log.d(TAG, "getBitmapFromURL: " + bitmap);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
             return bitmap;
-        }catch (IOException e){
-            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
             return null;
         }
     }
 
-    public class DownloadImage implements Runnable{
+    public class DownloadImage implements Runnable {
 
         @Override
         public void run() {
-            mImageView.setImageBitmap(getBitmapFromURL(urlToDownload));
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mImageView.setImageBitmap(getBitmapFromURL(urlToDownload));
+                }
+            });
         }
     }
 
