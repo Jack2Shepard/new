8d26c92f652ca12e0d44c13a3522d2e7f411295e
Shepard <downread@gmail.com>
2019-02-28 10:37:32 +0330

    Hey

--- app/src/main/java/com/jash/shepard/threadexample/MainActivity.java
+++ app/src/main/java/com/jash/shepard/threadexample/MainActivity.java
@@ -2,11 +2,9 @@ package com.jash.shepard.threadexample;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Handler;
-import android.os.StrictMode;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -15,34 +13,31 @@ import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.WeakHashMap;
 
 public class MainActivity extends AppCompatActivity {
     private String[] urls;
     private String urlToDownload;
     private ImageView mImageView;
     private Spinner mSpinner;
-    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StrictMode.ThreadPolicy policy = new StrictMode
-                .ThreadPolicy
-                .Builder()
-                .permitAll()
-                .build();
-        StrictMode.setThreadPolicy(policy);
+
 
         inits();
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 urlToDownload = urls[position];
-                Thread thread = new Thread(new DownloadImage());
-                thread.start();
+                new DownloadImageTask(MainActivity.this).execute(urlToDownload);
+                /*Thread thread = new Thread(new DownloadImage());
+                thread.start();*/
             }
 
             @Override
@@ -56,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
         mImageView = findViewById(R.id.image);
         mSpinner = findViewById(R.id.spinner);
         urls = getResources().getStringArray(R.array.urls);
-        mHandler = new Handler();
     }
 
     private Bitmap getBitmapFromURL(String src) {
@@ -66,24 +60,49 @@ public class MainActivity extends AppCompatActivity {
             connection.setDoInput(true);
             connection.connect();
             InputStream inputStream = connection.getInputStream();
-            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-            return bitmap;
+            return BitmapFactory.decodeStream(inputStream);
         } catch (IOException e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
             return null;
         }
     }
 
-    public class DownloadImage implements Runnable {
+/*    public class DownloadImage implements Runnable {
 
         @Override
         public void run() {
-            mHandler.post(new Runnable() {
+            final Bitmap bitmap = getBitmapFromURL(urlToDownload);
+            mImageView.post(new Runnable() {
                 @Override
                 public void run() {
-                    mImageView.setImageBitmap(getBitmapFromURL(urlToDownload));
+                    mImageView.setImageBitmap(bitmap);
                 }
             });
+        }
+    }*/
+
+    public static class DownloadImageTask extends AsyncTask <String,Void,Bitmap>{
+        private WeakReference<MainActivity> mWeakReference;
+
+        DownloadImageTask( MainActivity activity){
+            mWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            MainActivity activity = mWeakReference.get();
+            if(activity == null || activity.isFinishing()){
+                return null;
+            }
+            return activity.getBitmapFromURL(urls[0]);
+        }
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            MainActivity activity = mWeakReference.get();
+            if(activity == null || activity.isFinishing()){
+                return ;
+            }
+            activity.mImageView.setImageBitmap(bitmap);
         }
     }
 
